14f4374a6ae8a451f2e2cf4dfa9863507c945fa3
hoabao0809 <hoabao.0809@gmail.com>
2022-09-30 20:06:45 +0700

    fix download

--- controllers/covid.js
+++ controllers/covid.js
@@ -129,15 +129,8 @@ exports.getStaffsCovid = (req, res, next) => {
 
                   pdf
                     .create(document, {
-                      format: 'A4',
-                      orientation: 'portrait',
-                      border: '10mm',
-                      header: {
-                        height: '45mm',
-                      },
-                      footer: {
-                        height: '28mm',
-                      },
+                      format: 'Letter',
+                      orientation: 'landscape',
                       childProcessOptions: {
                         env: { OPENSSL_CONF: '/dev/null' },
                       },
